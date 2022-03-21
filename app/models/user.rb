a9785247c0e1e4ec5d75438de952bef7ffa9adeb
Seth Perna <gseth26@gmail.com>
2022-03-21 18:18:37 -0400

    complete all validations with specs for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,27 @@ class User < ApplicationRecord
   validates_format_of :email,:with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
   validates_presence_of :phone_number
   validates_presence_of :first_name
+  validates :phone_number, phone: true
+  before_save :normalize_phone
+
+
+  def formatted_phone
+    parsed_phone = Phonelib.parse(phone_number)
+    return phone_number if parsed_phone.invalid?
+
+    formatted =
+      if parsed_phone.country_code == "1"
+        parsed_phone.full_national
+      else
+        parsed_phone.full_international
+      end
+    formatted.gsub!(";", " x")
+    formatted
+  end
+
+  private
+
+  def normalize_phone
+    self.phone_number = Phonelib.parse(phone_number).full_e164.presence
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,27 @@
 require 'rails_helper'
 RSpec.describe User, type: :model do
   describe 'validations' do
-    it { should validate_presence_of :email }
-    it { should validate_presence_of :phone_number }
-    it { should validate_presence_of :first_name }
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:phone_number) }
+    it { should validate_presence_of(:first_name) }
+  end
+
+  describe 'instance and class methods' do
+    it '#formatted_phone' do
+      user_1 = User.create!(first_name: "Seth", last_name: "Perna", phone_number: "1(540)-347-9309", email: "gseth26@gmail.com")
+      user_2 = User.create!(first_name: "Seth", last_name: "Perna", phone_number: "248-447-9309", email: "gseth21@gmail.com")
+
+      user_3 = User.create(first_name: "Seth", last_name: "Perna", phone_number: "048-047-9309", email: "gseth27@gmail.com")
+
+      user_4 = User.create!(first_name: "Seth", phone_number: "248-447-9309", email: "gseth31@gmail.com")
+      user_5 = User.create(last_name: "bradley", phone_number: "248-447-9309", email: "gseth34@gmail.com")
+      expect(user_1.phone_number).to eq("+15403479309")
+      expect(user_2.phone_number).to eq("+12484479309")
+      expect(user_1.formatted_phone).to eq("(540) 347-9309")
+      expect(user_4).to be_a User
+      #testing that only valid phone numbers can be passed and that first name is required but not last name 
+      expect(user_3.save).to eq(false)
+      expect(user_5.save).to eq(false)
+    end
   end
 end
