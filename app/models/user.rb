ece39ac71a0f0d8e98fb20b0fe202596a06e3e96
Seth Perna <gseth26@gmail.com>
2022-03-21 17:36:20 -0400

    set up testing for valid email, testing for valid US phone number still required

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP },
+            :presence => {message: "can't be blank"},
+            :uniqueness => true
+  validates_format_of :email,:with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
+  validates_presence_of :phone_number
+  validates_presence_of :first_name
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+RSpec.describe User, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of :email }
+    it { should validate_presence_of :phone_number }
+    it { should validate_presence_of :first_name }
+  end
+end
