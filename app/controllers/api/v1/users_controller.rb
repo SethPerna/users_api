53a66a3754f0bbfcde02987e1198c11e7094ab57
Seth Perna <gseth26@gmail.com>
2022-03-22 08:01:21 -0400

    complete edge case testing for invalid attributes in CSV file

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,9 +4,20 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def create
-    CSV.foreach(params[:csv].path, headers: true) do |row|
-      User.create(row.to_h)
+    if params[:csv].present?
+      CSV.foreach(params[:csv].path, headers: true) do |row|
+        user = User.create(row.to_h)
+        if !user.save
+          return invalid_attributes
+        end
+      end
     end
     render json: {"data": { "message": "Successfully created"}}, status: 201
+  end
+
+  private
+
+  def invalid_attributes
+    render json: {"data": { "message": "Invalid Attributes"}}
   end
 end
